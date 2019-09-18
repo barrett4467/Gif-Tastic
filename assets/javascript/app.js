62726ffd662ad412172fb2b0e8aa607cfc8636bf
Rebecca Barrett <barrett4467@gmail.com>
2019-09-17 20:35:37 -0600

    populates all gifs as still started on click to change state

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,8 +29,7 @@ $("#find-book").on("click", function(){
 
 function showBookInfo() {
     var book = $(this).attr("data-name"); 
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + book + "&api_key=wmlyUx6osGzUCeMqFyU7NIXgqSuXtPET&limit=5"; 
-    
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + book + "&api_key=wmlyUx6osGzUCeMqFyU7NIXgqSuXtPET&limit=10"; 
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -38,27 +37,50 @@ function showBookInfo() {
     }).then(function(response){
         var gif = response.data.length;
         console.log("Gif: " + gif)
-
-      console.log(response);
-    //   $("#book-view").text(JSON.stringify(response));
+        
+        console.log(response);
+        //   $("#book-view").text(JSON.stringify(response));
         response.data.forEach(function(gif, i){
             var rating = response.data[i].rating;
             var img = $("<img>")
-            var gifAnimated = img.attr("src", response.data[i].images.fixed_height_small.url);
-            $("#book-view").html("<div id= 'info'></div>");
-            $("#info").append(gifAnimated);
+            var bookImage = img.attr("src", response.data[i].images.fixed_height_small_still.url);
+            img.attr({
+                "data-state": "still",
+                "data-still": response.data[i].images.fixed_height_small_still.url,
+                "data-animate": response.data[i].images.fixed_height_small.url,
+                "class": "gif"
+            });
+            
+            $("#book-view").append("<div id= 'info'></div>");
+            $("#info").append(bookImage);
             $("#info").append("<p> Rating: " + rating + "</p>");
-
+            
             console.log(response.data[i])
+            
         })
         
-
-    //   $("#info").append("<h2>" + title + "</h>");
-    //   $("#info").append(plot);
-      
-    //   $("#info").append("<img src="+ response.Poster + "/>")
+        
+        //   $("#info").append("<h2>" + title + "</h>");
+        //   $("#info").append(plot);
+        
+        //   $("#info").append("<img src="+ response.Poster + "/>")
         
     });
+            $(".gif").on("click", function(){
+                var state = $("#this").attr("data-state");
+                var stillURL = $(this).attr("data-still");
+                var animateURL = $(this).attr("data-animate");
+    
+                if (state === "still"){
+                    $(this).attr("src", animateURL);
+                    $(this).attr("data-state", "animate");
+    
+                } else{
+                    $(this).attr("src", stillURL);
+                    $(this).attr("data-state", "still"); 
+                }
+                
+            })
 
 
   }
